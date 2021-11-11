2d00db2915ba011ed81f91192f7f66c194097f2b
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2021-11-11 15:12:46 +0200

    Установка скриптов и решение задачи №1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onClickImg);
+
+function onClickImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const open = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
+  open.show();
+
+  window.addEventListener(`keydown`, onEscClose);
+
+  function onEscClose(event)
+  { if (event.code === 'Escape') { 
+    open.close()
+    window.removeEventListener(`keydown`, onEscClose);
+  }}
+}
+
+const newImgArray = ({preview, original, description}) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src = "${preview}"
+      data-source="${original}"
+      alt = "${description}"/>
+  </a>
+  </div>`;
+}
+
+const imageMarkup = galleryItems.map(newImgArray).join('');
+galleryRef.insertAdjacentHTML('beforeend', imageMarkup);
+
+console.log(imageMarkup);
