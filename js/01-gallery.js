407fdd1fb8dbda4ea168fc714fb4eacd4bcafbde
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2021-11-16 13:01:14 +0200

    исправление

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,29 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-const galleryRef = document.querySelector('.gallery');
+const galleryRef = document.querySelector(".gallery");
+const modalRef = document.querySelector("div.basicLightbox");
 
-galleryRef.addEventListener('click', onClickImg);
+galleryRef.addEventListener("click", onClickImg);
 
 function onClickImg(event) {
   event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
+  if (event.target.nodeName !== "IMG") {
     return;
   }
-  const open = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
+  const isActive = event.target.classList.add(".basicLightbox--visible");
+  const open = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
   open.show();
-
   window.addEventListener(`keydown`, onEscClose);
 
-  function onEscClose(event)
-  { if (event.code === 'Escape') { 
-    open.close()
+  if (!modalRef) {
     window.removeEventListener(`keydown`, onEscClose);
-  }}
+  }
+
+  function onEscClose(event) {
+    if (event.code === "Escape") {
+      open.close();
+      console.log(event);
+      window.removeEventListener(`keydown`, onEscClose);
+    }
+  }
 }
 
-const newImgArray = ({preview, original, description}) => {
+const newImgArray = ({ preview, original, description }) => {
   return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -33,7 +42,7 @@ const newImgArray = ({preview, original, description}) => {
       alt = "${description}"/>
   </a>
   </div>`;
-}
+};
 
-const imageMarkup = galleryItems.map(newImgArray).join('');
-galleryRef.insertAdjacentHTML('beforeend', imageMarkup);
+const imageMarkup = galleryItems.map(newImgArray).join("");
+galleryRef.insertAdjacentHTML("beforeend", imageMarkup);
